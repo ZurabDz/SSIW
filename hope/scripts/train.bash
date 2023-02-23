6a3f3f69ae78724a170f32d134207b271519ebcd
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-23 20:42:21 +0400

    running train bash script

---
+++ hope/scripts/train.bash
@@ -0,0 +1,2 @@
+python3 train_model.py --precision 32 --max_epochs 2 --batch_size 1 --root_data data/base/ \
+ --original_model_path data/model/segformer_7data.pth --label_desc_json_path data/labels.json
