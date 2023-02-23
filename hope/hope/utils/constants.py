c8312adbbeae1bd7a5197e0e6bfbb31c6b9cd3b6
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-23 15:33:51 +0400

    changed data dir

--- hope/hope/utils/constants.py
+++ hope/hope/utils/constants.py
@@ -1,2 +1,2 @@
 INITIAL_MODEL_PATH = '/home/penguin/SSIW/data/model/segformer_7data.pth'
-CLASS_DEFINITIONS_PATH = '/home/penguin/SSIW/Test_Minist/cmp_dataset_desc.json'
+CLASS_DEFINITIONS_PATH = '/home/penguin/SSIW/data/labels.json'
