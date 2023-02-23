56516335afa1d803dee1abcab7918eabad88b578
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-23 19:44:29 +0400

    script for download & extract

---
+++ hope/scripts/download_stuff.py
@@ -0,0 +1,8 @@
+import subprocess
+
+# token = "16pmWKSZYvmuocHCXbcMKA8HVlfKvFoet"
+# download_command = f"gdown {token}"
+unzip_command = "unzip data.zip"
+
+# subprocess.run(download_command.split())
+subprocess.run(unzip_command.split())
